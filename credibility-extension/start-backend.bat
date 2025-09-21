53d70916d501a5eb51a9d6456aafe55e5d990ae7
Prakriti Rathi <prakriti.rathi11@gmail.com>
2025-09-21 22:31:08 +0530

    remove bat files

--- credibility-extension/start-backend.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "c:\Users\prakr\OneDrive\Desktop\GenAI2\credibility-extension"
-node simple-backend.js
-pause
--- credibility-extension/start-server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting VerifiAI Gemini Backend Server...
-echo.
-cd /d "C:\Users\prakr\OneDrive\Desktop\GenAI2\credibility-extension"
-echo Current directory: %CD%
-echo.
-echo Starting server on port 3000...
-node gemini-backend.js
-pause
